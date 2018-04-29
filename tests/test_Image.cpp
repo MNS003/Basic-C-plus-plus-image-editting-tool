2d52745fab1ff90952ca50f13ebbc8bbf217de5e
Minenhle Sithole <n.minenhle.sithole@gmail.com>
2018-04-29 16:07:30 +0200

    Backup of test cases copied

---
+++ tests/test_Image.cpp
@@ -0,0 +1,28 @@
+#include "catch.hpp"
+#include "../lib/Image.h"
+
+#define CATCH_CONFIG_MAIN
+
+TEST_CASE("ctor with values"){
+    SECTION("Default ctor"){
+        
+    }
+    SECTION("blank image ctor"){
+        
+    }
+    SECTION("ctor with filename + load"){
+        
+    }
+    SECTION("copy ctor"){
+        
+    }
+    SECTION("copy assignment ctor"){
+        
+    }
+    SECTION("move ctor"){
+        
+    }
+    SECTION("move assignment ctor"){
+        
+    }
+}
